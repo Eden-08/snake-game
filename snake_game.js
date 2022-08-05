95ad4d42f103d75994faf4973f183e2c53cc6a68
eden-08 <kusmayantoeden@gmail.com>
2022-08-05 10:26:17 +0700

    Menambahkan beberapa fungsi baru

--- snake_game.js
+++ snake_game.js
@@ -38,6 +38,8 @@ class Snake{
 
 class Apple{
     constructor(){
+        console.log("apple")
+        console.log(snake.size)
         var isTouching;
         while(true){
             isTouching = false
@@ -48,19 +50,22 @@ class Apple{
                     isTouching = true
                 }
             }
-
+            console.log(this.x, this.y)
+            this.size = snake.size
+            this.color = "red"
             if(isTouching){
                 break;
             }
 
-            this.color = "pink"
-            this.size = snake.size
+            
+            
+            
         }
     }
 }
 
 var canvas = document.getElementById("canvas")
-var snake = new Snake();
+var snake = new Snake(20,20,20);
 var apple = new Apple();
 var canvasContext = canvas.getContext('2d');
 
@@ -79,7 +84,33 @@ function show(){
 
 
 function update(){
+    canvasContext.clearReact(0,0, canvas.width, canvas.height)
+    console.log("update")
+    snake.move()
+    eatApple()
+    checkHitwall();
+}
 
+function checkHitwall(){
+    var headTail = snake.tail[snake.tail.length -1]
+    if(headTail.x == - snake.size) {
+        headTail.x = canvas.width - snake.size
+    }else if(headTail.x == - canvas.width) {
+        headTail.x = 0
+    }else if(headTail.y == - snake.size) {
+        headTail.y = canvas.height - snake.size
+    }else if(headTail.y == - canvas.height) {
+        headTail.y = 0
+    }
+}
+
+
+function eatApple(){
+    if(snake.tail[snake.tail.length - 1].x == apple.x &&
+        snake.tail[snake.tail.length - 1].x == apple.y){
+            snake.tail[snake.tail.length] = {x:apple.x, y:apple.y}
+            apple = new Apple();
+        }
 }
 
 function draw(){
@@ -92,12 +123,30 @@ function draw(){
 
     canvasContext.font = "20px Arial"
     canvasContext.fillStyle = "#00FF42"
-    canvasContext.fillText("Score: ", (snake.tail.length +2),
-        canvas.width -120, 18);
-    createRect(apple.x, apple.y, apple.size, apple,size, apple.color)
+    canvasContext.fillText("Score: "+ (snake.tail.length -1),canvas.width -120, 18);
+    createRect(apple.x, apple.y, apple.size, apple,size, "pink")
 }
 
 function createRect(x,y,width, height,color){
     canvasContext.fillStyle = color
     canvasContext.fillRect(x,y,width,height)
 }
+
+window.addEventListener("keydown", (event)=>{
+    setTimeout(()=>{
+        if(event.keyCode == 37 && snake.rotateX != 1){
+            snake.rotateX = -1
+            snake.rotateY = 0;
+        }else if(event.keyCode == 38 && snake.rotateY != 1){
+            snake.rotateX = 0
+            snake.rotateY = -1;
+        }else if(event.keyCode == 39 && snake.rotateX != -1){
+            snake.rotateX = 1
+            snake.rotateY = 0;
+        }else if(event.keyCode == 40 && snake.rotateY != -1){
+            snake.rotateX = 0
+            snake.rotateY = 1;
+        }
+
+    })
+})
